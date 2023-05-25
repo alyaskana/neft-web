534b4bfecd2c56cce898f6d33ebc94be39cf5264
Alina Bychkova <ialina240200@gmail.com>
2023-05-25 19:19:53 +0200

    Show all things in stash

--- src/pages/game/components/stash/StashModal/StashModal.tsx
+++ src/pages/game/components/stash/StashModal/StashModal.tsx
@@ -12,84 +12,149 @@ import {
   Button,
 } from "@/share/components";
 import { $stash, $activeFish } from "@/pages/game/model";
-import { TCrop, TSeedStock, TStash } from "@/types/game";
 
 import s from "./StashModal.module.scss";
 import { eatCropFx } from "@/api/games";
 
 type TStashModal = TModal;
 
+type TCard = {
+  id: number;
+  count: number;
+  image: string;
+  key: string;
+  name: string;
+  description: string;
+  price?: number;
+  growing_time?: number;
+  experience?: number;
+  type: string;
+};
+
 export const StashModal: FC<TStashModal> = (props) => {
   const stash = useStore($stash);
   const activeFish = useStore($activeFish);
-  const [activeStashItem, setActiveStashItem] = useState<TSeedStock | TCrop>(
-    stash.seedStocks[0] || stash.crops[0]
-  );
+
+  const [activeCard, setActiveCard] = useState<TCard>(buildCards()[0]);
 
   useEffect(() => {
-    const activeItem = activeStashItem
-      ? activeStashItem
-      : stash.seedStocks[0] || stash.crops[0];
-    setActiveStashItem(activeItem);
+    const activeItem = activeCard ? activeCard : buildCards()[0];
+    setActiveCard(activeItem);
   }, [stash]);
 
-  const heandleEat = (crop: TCrop) => {
+  const heandleEat = (card: TCard) => {
+    const crop = stash.crops.find((crop) => crop.id == card.id)!;
     eatCropFx({ crop_id: crop.id, fish_id: activeFish.id });
   };
+
+  function buildCards() {
+    const crops = stash.crops.map((crop) => {
+      return {
+        id: crop.id,
+        count: crop.count,
+        image: crop.plant.image,
+        name: crop.plant.name,
+        description: crop.plant.description,
+        price: crop.plant.price,
+        experience: crop.plant.experience,
+        type: crop.type,
+        key: `${crop.type}-${crop.id}`,
+      };
+    });
+    const seedStocks = stash.seedStocks.map((seedStock) => {
+      return {
+        id: seedStock.id,
+        count: seedStock.count,
+        image: seedStock.plant.seed_image,
+        name: seedStock.plant.name,
+        description: seedStock.plant.description,
+        price: seedStock.plant.seed_price,
+        growing_time: seedStock.plant.growing_time,
+        type: seedStock.type,
+        key: `${seedStock.type}-${seedStock.id}`,
+      };
+    });
+    const instrumentStocks = stash.instrumentStocks.map((instrumentStock) => {
+      return {
+        id: instrumentStock.id,
+        count: instrumentStock.count,
+        image: instrumentStock.instrument.image,
+        name: instrumentStock.instrument.name,
+        description: instrumentStock.instrument.description,
+        price: instrumentStock.instrument.price,
+        type: instrumentStock.type,
+        key: `${instrumentStock.type}-${instrumentStock.id}`,
+      };
+    });
+    const mineralStocks = stash.mineralStocks.map((mineralStock) => {
+      return {
+        id: mineralStock.id,
+        count: mineralStock.count,
+        image: mineralStock.mineral.image,
+        name: mineralStock.mineral.name,
+        description: mineralStock.mineral.description,
+        price: mineralStock.mineral.price,
+        type: mineralStock.type,
+        key: `${mineralStock.type}-${mineralStock.id}`,
+      };
+    });
+    const recipeStocks = stash.recipeStocks.map((recipeStock) => {
+      return {
+        id: recipeStock.id,
+        count: recipeStock.count,
+        image: recipeStock.recipe.image,
+        name: recipeStock.recipe.name,
+        description: recipeStock.recipe.description,
+        experience: recipeStock.recipe.experience,
+        type: recipeStock.type,
+        key: `${recipeStock.type}-${recipeStock.id}`,
+      };
+    });
+
+    return [
+      ...crops,
+      ...seedStocks,
+      ...instrumentStocks,
+      ...mineralStocks,
+      ...recipeStocks,
+    ];
+  }
 
   return (
     <Modal {...props}>
       <Tabs>
         <TabPanel>
           <LeftPanel>
-            {Object.keys(stash).map((key) => {
-              return stash[key as keyof TStash].map((stashItem) => (
-                <MiniCard
-                  count={stashItem.count}
-                  image={
-                    stashItem.type == "crop"
-                      ? stashItem.plant.image
-                      : stashItem.plant.seed_image
-                  }
-                  active={
-                    stashItem.id == activeStashItem?.id &&
-                    stashItem.type == activeStashItem?.type
-                  }
-                  onClick={() => setActiveStashItem(stashItem)}
-                  key={`${stashItem.type}-${stashItem.id}`}
-                />
-              ));
-            })}
+            {buildCards().map((Card) => (
+              <MiniCard
+                count={Card.count}
+                image={Card.image}
+                active={activeCard?.key == Card.key}
+                onClick={() => setActiveCard(Card)}
+                key={Card.key}
+              />
+            ))}
           </LeftPanel>
           <RightPanel>
-            {activeStashItem && activeStashItem.type == "crop" && (
+            {activeCard && (
               <>
                 <Card
-                  image={activeStashItem.plant.image}
-                  name={activeStashItem.plant.name}
-                  description={activeStashItem.plant.description}
-                  rarity={activeStashItem.plant.rarity}
-                  sellingPrice={activeStashItem.plant.price}
-                  experience={activeStashItem.plant.experience}
+                  image={activeCard.image}
+                  name={activeCard.name}
+                  description={activeCard.description}
+                  seedPrice={activeCard.price}
+                  growingTime={activeCard.growing_time}
                 />
-                <Button
-                  onClick={() => {
-                    heandleEat(activeStashItem);
-                  }}
-                >
-                  съесть
-                </Button>
+                {activeCard.type == "crop" && (
+                  <Button
+                    onClick={() => {
+                      heandleEat(activeCard);
+                    }}
+                  >
+                    съесть
+                  </Button>
+                )}
               </>
-            )}
-            {activeStashItem && activeStashItem.type == "seed_stock" && (
-              <Card
-                image={activeStashItem.plant.seed_image}
-                name={activeStashItem.plant.name}
-                description={activeStashItem.plant.description}
-                rarity={activeStashItem.plant.rarity}
-                seedPrice={activeStashItem.plant.seed_price}
-                growingTime={activeStashItem.plant.growing_time}
-              />
             )}
           </RightPanel>
         </TabPanel>
